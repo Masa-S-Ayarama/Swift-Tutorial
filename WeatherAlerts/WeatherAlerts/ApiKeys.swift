df4e7e53176dbefe9446ab3eb860940864537047
Pan-Masa <nasmac@NasMacs-MacBook-Air.local>
2016-01-08 13:23:59 +0700

    added ApiKeys.swift

---
+++ WeatherAlerts/WeatherAlerts/ApiKeys.swift
@@ -0,0 +1,21 @@
+//
+//  ApiKeys.swift
+//  WeatherAlerts
+//
+//  Created by Nas@Mac on 8/1/59.
+//  Copyright © พ.ศ. 2559 Nas@Mac. All rights reserved.
+//
+
+import Foundation
+
+func valueForAPIKey(keyname keyname:String) -> String {
+    // Credit to the original source for this technique at
+    // http://blog.lazerwalker.com/blog/2014/05/14/handling-private-api-keys-in-open-source-ios-apps
+    let filePath = NSBundle.mainBundle().pathForResource("ApiKeys", ofType:"plist")
+    let plist = NSDictionary(contentsOfFile:filePath!)
+    
+    let value:String = plist?.objectForKey(keyname) as! String
+    return value
+}
+
+let clientID = valueForAPIKey(keyname:"API_CLIENT_ID")
